84a814b39e350782e03f971af21a57f4f174782d
Andi Miller <andi@andimiller.net>
2024-05-30 22:45:30 +0100

    add a test

--- src/test/scala/net/andimiller/clowder/Placeholder.scala
+++
@@ -1,5 +0,0 @@
-package net.andimiller.clowder
-
-class Placeholder {
-
-}
--- src/test/scala/net/andimiller/clowder/anomalydetection/IsolationForestsSpec.scala
+++ src/test/scala/net/andimiller/clowder/anomalydetection/IsolationForestsSpec.scala
@@ -17,6 +17,25 @@ class IsolationForestsSpec extends munit.FunSuite with ScalaCheckSuite {
     )
   }
 
+  property("should find an extra value added into a range") {
+    implicit val values: Arbitrary[Vector[Int]] = Arbitrary {
+      val value = Gen.choose(0, 1000)
+      for {
+        h0 <- value
+        h1 <- value
+        h2 <- value
+      } yield Vector(h0, h1, h2)
+    }
+    implicit val extraValue: Arbitrary[Int]     = Arbitrary(Gen.choose(2500, 3000))
+    forAll { (ints: Vector[Int], extra: Int) =>
+      implicit val r = new Random(1234)
+      val alg        = new IsolationForests[Int](samples = 256, stdDevs = 1)
+      alg.findOutliers(
+        r.shuffle(ints.appended(extra))
+      ) == Vector(extra)
+    }
+  }
+
   property("should run with many longs") {
     implicit val r                            = new Random(1234)
     val alg                                   = new IsolationForests[Long](samples = 32, stdDevs = 1)
